cbac305e7661929b9818a8a49312ff2e69009172
Acendy <zentokaishi@gmail.com>
2019-11-05 23:10:50 -0500

    merge

--- app/src/main/java/com/example/tp/MainActivity.java
+++ app/src/main/java/com/example/tp/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.tp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
